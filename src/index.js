fe8ca1ec4491047aa7b627dc0f63fc5b0d690c35
VishwasSahu <vishwas.sahu@celigo.com>
2025-04-18 17:59:27 +0530

    Add stack class implementation and export in index.js

--- src/index.js
+++ src/index.js
@@ -4,3 +4,4 @@
  * @description This file serves as the main entry point for the library, exporting all necessary modules and components.
  */
 export { vector } from './vector.js'
+export { stack } from './stack.js'
